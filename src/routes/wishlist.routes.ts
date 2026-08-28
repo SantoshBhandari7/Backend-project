6980211690120d2ae61ca00014c25947d73097d6
Santosh Bhandari <santooshbhandari88@gmail.com>
2026-08-28 20:02:15 +0545

    remanage

--- src/routes/wishlist.routes.ts
+++ src/routes/wishlist.routes.ts
@@ -5,11 +5,9 @@ import { authenticate } from "../middlewares/auth.middleware";
 import { Role } from "../@types/enum.types";
 
 const router = express.Router();
-
 router.get("/", authenticate([Role.USER]), getWishList);
 
 router.post("/", authenticate([Role.USER]), createWish);
 
 router.delete("/:productId", authenticate([Role.USER]), removeWish);
-
 export default router;
